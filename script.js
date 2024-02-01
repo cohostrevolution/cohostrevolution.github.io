f1b024161e36ae26373aa58a2cb5df1843813e76
cohostrevolution <158428969+cohostrevolution@users.noreply.github.com>
2024-02-01 10:45:29 +0530

    Published website
    CO HOST REVOLUTION

---
+++ script.js
@@ -0,0 +1,18 @@
+
+var myButton2 = document.getElementById('home');
+myButton2.addEventListener('click', function() {
+var newWebsiteUrl2 = 'index.html';
+window.location.href = newWebsiteUrl2;
+})
+
+var myButton3 = document.getElementById('info');
+myButton3.addEventListener('click', function() {
+var newWebsiteUrl3 = 'info.html';
+window.location.href = newWebsiteUrl3;
+})
+
+var myButton4 = document.getElementById('news');
+myButton4.addEventListener('click', function() {
+var newWebsiteUrl4 = 'news.html';
+window.location.href = newWebsiteUrl4;
+})
